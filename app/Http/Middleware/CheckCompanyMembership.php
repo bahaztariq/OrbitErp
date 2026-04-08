f4b1d3b5f9d237c05407641c5e890f325b00f3f8
bahaztariq <bahaztariq@gmail.com>
2026-04-08 16:12:28 +0100

    feat: add membership middleware

---
+++ app/Http/Middleware/CheckCompanyMembership.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\Company;
+
+class CheckCompanyMembership
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $company = $request->route('company');
+
+        if ($company instanceof Company) {
+            if (!$company->isMember(auth()->id())) {
+                return response()->json([
+                    'message' => 'You are not authorized to access this company',
+                ], 403);
+            }
+        } elseif (is_string($company)) {
+            $companyModel = Company::where('slug', $company)->first();
+            if ($companyModel && !$companyModel->isMember(auth()->id())) {
+                return response()->json([
+                    'message' => 'You are not authorized to access this company',
+                ], 403);
+            }
+        }
+
+        return $next($request);
+    }
+}
