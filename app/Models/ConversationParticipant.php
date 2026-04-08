411ea8a8f6b1d43132e0fb0fa82fa85adcc37efb
bahaztariq <bahaztariq@gmail.com>
2026-04-08 17:19:06 +0100

    Add ConversationParticipant model

---
+++ app/Models/ConversationParticipant.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ConversationParticipant extends Model
+{
+    /** @use HasFactory<\Database\Factories\ConversationParticipantFactory> */
+    use HasFactory, SoftDeletes;
+
+
+    protected $fillable = [
+        'conversation_id',
+        'user_id',
+    ];
+
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
